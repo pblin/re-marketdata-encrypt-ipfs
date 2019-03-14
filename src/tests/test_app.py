b2d761c3d8d3b851e014a3afd7b80c808530b269
Lin, Bernard <bernardlin@outlook.com>
2019-03-14 12:01:16 -0400

    update tests

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -8,7 +8,7 @@ class test_api(unittest.TestCase):
         self.app = app.app.test_client()
         self.app.testing = True
 
-    def test_smaple_status_code(self):
+    def test_sample_status_code(self):
         assert self.app.get('/sample/nyc_lot/test123').status_code == 200
 
     def test_search_status_code(self):
