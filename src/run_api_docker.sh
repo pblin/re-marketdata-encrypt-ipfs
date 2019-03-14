ef03c68a799ea63668f674a599020a5cb050c4a2
Lin, Bernard <bernardlin@outlook.com>
2019-03-14 18:00:00 -0400

    update docker run script

--- src/run_api_docker.sh
+++ src/run_api_docker.sh
@@ -2,6 +2,6 @@
 CONTAINER_IMAGE=$1
 if [ `docker ps | grep searchapi | wc -l` -gt 0 ]; then
 	docker stop searchapi
-	docker container prune -f 
 fi
+docker container prune -f 
 docker run -d -p 8082:8082 --name=searchapi $CONTAINER_IMAGE
